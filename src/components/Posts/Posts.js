f4fa535e0e58a2a589c66216db86da40b95d6a1d
N3tw0rk <khoirrurrohman@gmail.com>
2021-05-25 22:59:10 +0700

    show all data from database

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,3 +1,4 @@
+import { CircularProgress, Grid } from "@material-ui/core";
 import React from "react";
 import { useSelector } from "react-redux";
 
@@ -7,12 +8,22 @@ import useStyles from "./styles";
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
-  console.log(posts);
-  return (
-    <>
-      <h1>Posts Components</h1>
-      <Post />
-    </>
+
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.mainContainer}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
